8f92341b1cbbed65be160b11425c79c0227ad752
kaiomarian <161875538+kaiomarian@users.noreply.github.com>
2025-10-23 11:24:08 -0300

    Add files via upload

---
+++ sql cdmusica.sql
@@ -0,0 +1,44 @@
+create database cadastro_cd;
+use cadastro_cd;
+create table gravadora(
+cod_gra int primary key not null auto_increment,
+nome_gra varchar(60),
+endereço varchar(100),
+telefone varchar(10),
+contato varchar(20),
+url varchar(80));
+
+create table cd(
+cod_cd int primary key not null auto_increment,
+cod_gravadora int,
+nome varchar(100),
+preço decimal (14,2),
+data_lanc datetime,
+cd_indicado int);
+
+create table musica(
+cod_mus int primary key not null auto_increment,
+nome_mus varchar(100),
+dur_mus decimal (14,2));
+
+create table faixa(
+cod_cdfaixa int,
+cod_musfaixa int,
+numero_faixa int);
+
+ alter table faixa
+ add constraint fk_cd
+ foreign key (cod_cdfaixa) references cd (cod_cd);
+ 
+ alter table faixa 
+ add constraint fk_musica
+ foreign key (cod_musfaixa) references musica (cod_mus);
+ 
+ alter table cd 
+ add constraint fk_gravadora
+ foreign key (cod_gravadora) references gravadora (cod_gra);
+ 
+ alter table gravadora modify nome_gra varchar(80);
+ alter table cd drop column cd_indicado;
+ alter table cd change nome Título varchar (100);
+
