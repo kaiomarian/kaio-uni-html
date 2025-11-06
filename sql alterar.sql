3f7a43cdafd1196a7c44308adf73572b6fdbfc44
kaiomarian <161875538+kaiomarian@users.noreply.github.com>
2025-11-06 11:47:21 -0300

    Update sql alterar.sql

--- sql alterar.sql
+++ sql alterar.sql
@@ -1,12 +1,12 @@
 create database vendinhaZe;
 use vendinhaZe;
 create table clientes ( 
-id Int-, 
+id Int primary key auto_increment not null, 
 nome VARCHAR(100), 
 email VARCHAR(100));
 create table pedidos ( 
- id INT,
- cliente_id INT,
+ id INT primary key auto_increment not null,
+ cliente_id INT not null,
  produto VARCHAR(100),
  qnt INT );
  alter table pedidos add data_pedidos date;
@@ -16,11 +16,37 @@ create table pedidos (
  alter table clientes add cidade varchar(100);
  alter table pedidos add val_pedido decimal (10,2);
  alter table clientes change endereço edr varchar(100);
- alter table pedidos change cliente_id cli_id int;
+ alter table pedidos change cli_id cliente_id int;
  alter table clientes add primary key (id);
  alter table pedidos add primary key (id);
  alter table clientes modify id int auto_increment not null;
+ create table func(
+ id integer not null primary key,
+ nome varchar(50),
+ salario numeric(10,2)
+ );
+
  alter table pedidos 
  add constraint fk_cliente 
  foreign key (cli_id) references clientes (id);
+ 
+ 
+ 
+insert into clientes(id,nome,email,UF,cidade,edr)
+values (12,"caliborn", "siids@gmail.com", "CU", "hell","rua bosta"),
+(13, "calliope", '', "Ur", "unggu","rua gswgrgrw");
+update clientes 
+set cidade="muguengue", edr="avenida jesus" where id = 11;
+insert into func(id,nome,salario) values
+(4,"fb",2034),
+(5,"fwe",3694),
+(6,"rsjh",1102);
+select nome, salario + 200 as aumento from func;
+select avg(salario) as media,
+sum(salario) as total,
+max(salario) as maior,
+min(salario) as menor
+from func;
 
+delete from func
+where id = 3;
